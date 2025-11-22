cb6846597dee624fd53cd80a397958bed592a28f
fame372 <70764830+fame372@users.noreply.github.com>
2025-11-22 10:04:40 -0700

    merged the files as best as I could.

--- project-chingu-map/src/App.jsx
+++ project-chingu-map/src/App.jsx
@@ -1,15 +1,16 @@
-
-
 import './App.css'
 import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
-
-
+import Hero from './components/Hero.jsx'
+import TeamCard from './components/TeamCard.jsx'
+  
 function App() {
   
   return (
     <>
       <Header />
+      <Hero /> 
+      <TeamCard />
       <Footer />
     </>
   )
